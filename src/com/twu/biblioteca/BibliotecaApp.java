3e06d8997f8f6561d152319be2dd4979de7b0045
Katrina <zhaoyanlin2016@gmail.com>
2018-05-10 00:29:41 +0800

    feat: display main menu

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,14 +1,23 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, world!");
+        displayWelcomeMessage();
+        displayMainMenu();
+
+        Scanner scanner = new Scanner(System.in);
     }
 
 
     public static String displayWelcomeMessage() {
         return "Welcome to the Biblioteca!";
+    }
+
+    public static String displayMainMenu() {
+        return "Main menu\n1. List Books";
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -7,12 +7,14 @@ import static org.junit.Assert.assertEquals;
 public class ExampleTest {
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
-    }
-
-    @Test
     public void should_display_welcome_message() {
         assertEquals("Welcome to the Biblioteca!", BibliotecaApp.displayWelcomeMessage());
     }
+
+    @Test
+    public void should_display_main_menu() {
+         assertEquals("Main menu\n1. List Books", BibliotecaApp.displayMainMenu());
+    }
+
+
 }
